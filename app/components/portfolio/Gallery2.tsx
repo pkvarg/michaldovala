e09435c70110cb7ea100621d45f686d9a198c369
peter varga <pkvarg@yahoo.se>
2024-05-04 21:21:46 +0200

    cv

--- app/components/portfolio/Gallery2.tsx
+++ app/components/portfolio/Gallery2.tsx
@@ -5,16 +5,27 @@ import Image from 'next/image';
 const Gallery2 = () => {
   return (
     <>
-      <div className="hidden h-screen flex-col bg-[#060085] lg:flex">
+      <div className="relative hidden h-screen flex-col bg-[#060085] lg:flex">
         <div className="pb-8 lg:pt-[2%] 2xl:pt-[3%]">
           <Header />
         </div>
+
         <div>
+          <div className="absolute right-[10%] top-[31%] hidden lg:flex 2xl:right-[5%] 2xl:top-[30%]">
+            <div>
+              <div className="ml-[10px] h-[85px] w-[290px] bg-[#edd900]"></div>
+              <div className=" -mt-[93px] mr-4 bg-[#ffffff] px-4 py-2 text-[30px]">
+                <h1 className="text-[45px] font-black uppercase text-green-900">
+                  Cv na mieru
+                </h1>
+              </div>
+            </div>
+          </div>
           <Image
             height={1000}
             width={1000}
             alt="michal dovala"
-            src={'/gallery002.webp'}
+            src={'/gallery002.png'}
             className="w-screen"
           />
         </div>
